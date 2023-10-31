5967903325b4b02b44170edc9624c2bdfc42c624
Moromon <lmoralvxd@gmail.com>
2023-10-31 17:10:54 +0100

    Comentarios y bug arreglado
    +Comentarios
    +Bug arreglado con el pase de preguntas

--- Moviles/app/src/main/java/com/example/moviles/MainActivity.kt
+++ Moviles/app/src/main/java/com/example/moviles/MainActivity.kt
@@ -12,16 +12,18 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
-    private val questions = arrayOf("1,2,3")
-
-    private val options = arrayOf(arrayOf("q", "b", "c", "o"), arrayOf("a,c,d,j"))
-
+    //Array de preguntas temporal
+    private val questions = arrayOf("1","2")
+    //Array de opciones temporal
+    private val options = arrayOf(arrayOf("q", "b", "c", "o"), arrayOf("a","c","d","j"))
+    //Array de repsuestas correctas temporal
     private val correctAnswers = arrayOf(1, 0)
-
+    //Array de contador de pregntas
     private var currentQuestionIndex = 0
-
+    //Array de puntos
     private var score = 0
 
+    //Funicon al inicializar la aplicacion
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -46,7 +48,7 @@ class MainActivity : AppCompatActivity() {
             restartQuiz()
         }
     }
-
+    //Funcion al pulsar el boton correcto
     private fun correctButtonColors(buttonIndex: Int) {
         when (buttonIndex) {
             0 -> binding.option1Button.setBackgroundColor(Color.GREEN)
@@ -58,7 +60,7 @@ class MainActivity : AppCompatActivity() {
             3 -> binding.option4Button.setBackgroundColor(Color.GREEN)
         }
     }
-
+    //Funcion al pulsar el boton incorrecto
     private fun wrongButtonColors(buttonIndex: Int) {
         when (buttonIndex) {
             0 -> binding.option1Button.setBackgroundColor(Color.RED)
@@ -70,20 +72,20 @@ class MainActivity : AppCompatActivity() {
             3 -> binding.option4Button.setBackgroundColor(Color.RED)
         }
     }
-
+    //Funcion para resetear el color de los botones
     private fun resetButtonColors() {
         binding.option1Button.setBackgroundColor(Color.BLUE)
         binding.option2Button.setBackgroundColor(Color.BLUE)
         binding.option3Button.setBackgroundColor(Color.BLUE)
         binding.option4Button.setBackgroundColor(Color.BLUE)
     }
-
+    //Funcion para mostrar el resultado
     private fun showResults() {
         Toast.makeText(this, "Your score: $score out of ${questions.size}", Toast.LENGTH_LONG)
             .show()
         binding.restartButton.isEnabled = true;
     }
-
+    //Funcion para cambiar de pregunta
     private fun displayQuestion() {
         binding.questionText.text = questions[currentQuestionIndex]
         binding.option1Button.text = options[currentQuestionIndex][0]
@@ -92,7 +94,7 @@ class MainActivity : AppCompatActivity() {
         binding.option4Button.text = options[currentQuestionIndex][3]
         resetButtonColors()
     }
-
+    //Funcion general que dictamina el uso general del cuestionario
     private fun checkAnswer(selectedAnswerIndex: Int) {
 
         val correctAnswerIndex = correctAnswers[currentQuestionIndex]
@@ -104,16 +106,15 @@ class MainActivity : AppCompatActivity() {
             wrongButtonColors(selectedAnswerIndex)
             correctButtonColors(correctAnswerIndex)
         }
-        if (currentQuestionIndex < questions.size - 1) {
+        if (currentQuestionIndex < questions.size -1 ) {
             currentQuestionIndex++
-            binding.questionText.postDelayed({ displayQuestion() }, 1000)
-
+            binding.questionText.postDelayed({ displayQuestion()}, 1000)
         } else {
             showResults()
         }
 
     }
-
+    //Funcion que resetea el loop general
     private fun restartQuiz(){
         currentQuestionIndex = 0
         score = 0
